8bfe2fbf2bba53239e6f63e6bb280c13f63e6722
maxx1256 <maxi17y@live.com>
2021-12-01 19:25:36 -0800

    musicbatch settings

--- musicbatch/run.bat
+++ musicbatch/run.bat
@@ -1,1 +1,3 @@
-"C:\Program Files (x86)\Python35-32\python.exe" musicbatch.py config.ini 
+#"C:\Program Files (x86)\Python35-32\python.exe" musicbatch.py config.ini 
+
+python.exe musicbatch.py config.ini 
