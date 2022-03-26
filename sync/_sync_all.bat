a42bf38299dc4427baf9b7b52c4aec27603d24cb
maxx1256 <maxi17y@live.com>
2022-03-26 15:58:19 -0700

    Update _sync_all.bat

--- sync/_sync_all.bat
+++ sync/_sync_all.bat
@@ -1,4 +1,5 @@
 @echo off
+
 call _backup_do.bat sync.007.log sync.008.log
 call _backup_do.bat sync.006.log sync.007.log
 call _backup_do.bat sync.005.log sync.006.log
@@ -8,9 +9,8 @@ call _backup_do.bat sync.002.log sync.003.log
 call _backup_do.bat sync.001.log sync.002.log
 call _backup_do.bat sync.log     sync.001.log
 
-echo ------------------
 call d:\_sync.bat
-echo ------------------
 call e:\_sync.bat
-echo ------------------ all done
+
+if '%COMPUTERNAME%'=='FLEXO' xcopy * D:\pub\sync_settings\ /EXCLUDE:_exclude.txt 
 
